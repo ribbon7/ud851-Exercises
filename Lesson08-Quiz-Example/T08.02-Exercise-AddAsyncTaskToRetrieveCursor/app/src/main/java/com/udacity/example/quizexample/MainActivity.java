db08731f24c0b53ff4e6d6c05c32897f60276015
Connor Earl <connor@manonfireent.com>
2017-03-30 22:56:54 -0600

    Complete T08.02

--- Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -16,10 +16,16 @@
 
 package com.udacity.example.quizexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
  * Gets the data from the ContentProvider and shows a series of flash cards.
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private int mCurrentState;
 
     // TODO (3) Create an instance variable storing a Cursor called mData
+    private Cursor mCursor;
     private Button mButton;
 
     // This state is when the word definition is hidden and clicking the button will therefore
@@ -51,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         mButton = (Button) findViewById(R.id.button_next);
 
         // TODO (5) Create and execute your AsyncTask here
+        new FetchTermsTask().execute();
     }
 
     /**
@@ -94,5 +102,21 @@ public class MainActivity extends AppCompatActivity {
     // TODO (2) In the doInBackground method, write the code to access the DroidTermsExample
     // provider and return the Cursor object
     // TODO (4) In the onPostExecute method, store the Cursor object in mData
+    private class FetchTermsTask extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            ContentResolver contentResolver = getContentResolver();
+            Cursor result = contentResolver.query(DroidTermsExampleContract.CONTENT_URI,
+                    null, null, null, null);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+            mCursor = cursor;
+        }
+    }
+
 
 }
